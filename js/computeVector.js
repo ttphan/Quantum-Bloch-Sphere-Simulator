07b1a8aaaf7d07080d49c7b31741813fdcf06d28
annemiekie <annemiekie.v@gmail.com>
2015-06-05 14:22:13 +0200

    noise functions matrix computations

--- js/computeVector.js
+++ js/computeVector.js
@@ -10,13 +10,13 @@ var statePlus = math.multiply(1/Math.sqrt(2),[[1],[1]]);
 var stateMin = math.multiply(1/Math.sqrt(2),[[1],[-1]]);
 var stateOne = [[0],[1]];
 var stateZero = [[1],[0]];
-
+var maxMixed = [[0.5, 0],[0, 0.5]];
 
 function getVector(densMatrix){
       //var densMatrix = [[a,b],[c,d]];
       var xCoor = trace(math.multiply(densMatrix,gateX));
-      var yCoor = trace(math.multiply(densMatrix,gateZ));
-      var zCoor = trace(math.multiply(densMatrix,gateY));
+      var yCoor = trace(math.multiply(densMatrix,gateY));
+      var zCoor = trace(math.multiply(densMatrix,gateZ));
 
       var vector = new THREE.Vector3( xCoor, yCoor, zCoor );
       return vector;
@@ -26,9 +26,13 @@ function trace(matrix){
       return math.add(matrix[0][0],matrix[1][1]);
 }
 
-function stateToDensityMatrix(state) {
+function conjugateTranspose(matrix) {
+      return math.conjugate(math.transpose(matrix));
+}
+
+function stateToDens(state) {
       //state = [a,b];
-      return math.multiply(state,state.transpose());
+      return math.multiply(state,transpose(state));
 }
 
 function tensorProduct(state1,state2) {
@@ -48,4 +52,25 @@ function getPhaseShiftGate(shift) {
 
 function applyGate(state,gate) {
       return math.multiply(gate,state);
+}
+
+function depolNoise(densMatrix, r) {
+      return math.multiply(r,densMatrix) + math.multiply((1-r),maxMixed);
+}
+
+function dephaseNoiseX(densMatrix,r) {
+      return math.multiply(r,densMatrix) 
+            + math.multiply((1-r),math.multiply(math.multiply(gateX,densMatrix),gateX));
+}
+
+function dephaseNoiseZ(densMatrix,r) {
+      return math.multiply(r,densMatrix) 
+            + math.multiply((1-r),math.multiply(math.multiply(gateZ,densMatrix),gateZ));
+}
+
+function ampDampNoise(densMatrix,r) {
+      a0 = stateToDens(stateZero) + math.multiply(Math.sqrt(r),stateToDens(stateOne));
+      a1 = math.multiply(Math.sqrt(1-r), math.multiply(stateZero,stateOne.transpose()));
+      return math.multiply(math.multiply(a0,densMatrix), math.conjugateTranspose(a0)) + 
+            math.multiply(math.multiply(a1,densMatrix), math.conjugateTranspose(a1));
 }
