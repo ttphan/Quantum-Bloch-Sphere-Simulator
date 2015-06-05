8721e9297c6eff7bca4cf0ed32b6821ec26f4ff8
annemiekie <annemiekie.v@gmail.com>
2015-06-05 15:40:45 +0200

    More functions computeVector

--- js/computeVector.js
+++ js/computeVector.js
@@ -3,11 +3,11 @@ var complexi = math.complex('i');
 var gateX = [[0,1],[1,0]];
 var gateY = [[0,math.complex('-i')],[math.complex('i'),0]];
 var gateZ = [[1,0],[0,-1]];
-var gateH = math.multiply(1/Math.sqrt(2),[[1,1],[1,-1]]);
+var gateH = math.multiply(1/math.sqrt(2),[[1,1],[1,-1]]);
 var gateSWAP = [[1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]];
 var gateCNOT = [[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]];
-var statePlus = math.multiply(1/Math.sqrt(2),[[1],[1]]);
-var stateMin = math.multiply(1/Math.sqrt(2),[[1],[-1]]);
+var statePlus = math.multiply(1/math.sqrt(2),[[1],[1]]);
+var stateMin = math.multiply(1/math.sqrt(2),[[1],[-1]]);
 var stateOne = [[0],[1]];
 var stateZero = [[1],[0]];
 var maxMixed = [[0.5, 0],[0, 0.5]];
@@ -24,6 +24,21 @@ function getVector(densMatrix){
 
 function trace(matrix){
       return math.add(matrix[0][0],matrix[1][1]);
+}
+
+function getEigenvalues(matrix) {
+      a = matrix[0][0];
+      b = matrix[0][1];
+      c = matrix[1][0];
+      d = matrix[1][1];
+      part1 = math.multiply(-1,math.add(a,d));
+      part2 = math.pow(math.add(a,d),2);
+      part3 = math.substract(math.multiply(a,d), math.multiply(c,d));
+      lambda1 = math.add(part1, math.sqrt(math.substract(part2,part3));
+      lambda1 = math.multiply(0.5,lambda1);
+      lambda2 = math.substract(part1, math.sqrt(math.substract(part2,part3));
+      lambda2 = math.multiply(0.5,lambda2);
+      return [lambda1, lambda2];
 }
 
 function conjugateTranspose(matrix) {
@@ -69,12 +84,35 @@ function dephaseNoiseZ(densMatrix,r) {
 }
 
 function ampDampNoise(densMatrix,r) {
-      a0 = stateToDens(stateZero) + math.multiply(Math.sqrt(r),stateToDens(stateOne));
-      a1 = math.multiply(Math.sqrt(1-r), math.multiply(stateZero,stateOne.transpose()));
+      a0 = stateToDens(stateZero) + math.multiply(math.sqrt(r),stateToDens(stateOne));
+      a1 = math.multiply(math.sqrt(1-r), math.multiply(stateZero,stateOne.transpose()));
       return math.multiply(math.multiply(a0,densMatrix), math.conjugateTranspose(a0)) + 
             math.multiply(math.multiply(a1,densMatrix), math.conjugateTranspose(a1));
 }
 
 function isPure(densMatrix) {
       return trace(math.multiply(densMatrix, math.transpose(densMatrix))) == 1;
+}
+
+function isValidTrace(densMatrix) {
+      return trace(densMatrix) == 1;
+}
+
+function isValidHermitian(densMatrix) {
+      return densMatrix == conjugateTranspose(densMatrix);
+}
+
+function isValidEigenvalues(densMatrix) {
+      eigval = getEigenvalues(densMatrix);
+      return ((eigval[0] <= 1) && (eigval[0] >= 0) && (eigval[1] <= 1) && (eigval[1] >= 0));
+}
+
+function isHamiltonianTracd(matrix) {
+      return trace(matrix) == 0;
+}
+
+function getPadeApprox(matrix) {
+
+      denom = 0;
+      num = 0;
 }
